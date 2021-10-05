e10880e16415386ae3f96a7df9038d18bff18cd4
GangH <rhrkdgus1020@naver.com>
2021-10-05 12:33:19 +0900

    "using num count"

---
+++ Beakjoon/2577/2577.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b, c, n, cnt[10]={0};
+    cin>>a>>b>>c;
+    n=a*b*c;
+    while(n!=0){
+        cnt[n%10]++;
+        n/=10;
+    }
+    for(int i=0;i<=9;i++){
+        cout<<cnt[i]<<endl;
+    }
+}
