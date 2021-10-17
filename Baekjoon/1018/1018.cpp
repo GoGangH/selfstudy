f4527615be37f2e7f126c37e70cd96eed8bce70f
GangH <rhrkdgus1020@naver.com>
2021-10-17 17:02:01 +0900

    "file name change"

---
+++ Baekjoon/1018/1018.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, m, til[50][50];
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++) cin>>til[i][j];
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<)
+    }
+}
