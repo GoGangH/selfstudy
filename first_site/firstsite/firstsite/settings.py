d80ef095128617e26028d5de65e29233da99cbfe
GangH <rhrkdgus1020@naver.com>
2022-03-04 14:39:40 +0900

    'set litesql > mysql and make home'

--- first_site/firstsite/firstsite/settings.py
+++ first_site/firstsite/firstsite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from re import M
+import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-83aju9v#83a%7bb05ut#k^51gymdv5tb7zng=j3yk^ip%48d()'
+#SECRET_KEY = 'django-insecure-83aju9v#83a%7bb05ut#k^51gymdv5tb7zng=j3yk^ip%48d()'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls'
 ]
 
 MIDDLEWARE = [
@@ -72,14 +75,18 @@ WSGI_APPLICATION = 'firstsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sql',
     }
 }
+'''
+DATABASES = my_settings.DATABASES
+SECRET_KEY =  my_settings.SECRET
 
+#my_settings.py에 담어뒀음
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
